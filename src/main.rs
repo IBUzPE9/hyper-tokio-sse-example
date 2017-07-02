6a2086eb5d48bfda3b68ff4470362c17cd684870
Alex <alex@drop.it>
2017-07-03 00:45:34 +0300

    minor improvements

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,21 @@ extern crate pretty_env_logger;
 extern crate tokio_core;
 extern crate tokio_io;
 
-use futures::future::{FutureResult, BoxFuture, Either};
+use futures::future::{BoxFuture, Either, ok as future_ok};
 use futures::{Stream,Future,Sink,Poll};
 use futures::sync::mpsc;
 use futures::future::{loop_fn, Loop};
 
-use tokio_core::reactor::{Core, Handle, Timeout};
+use tokio_core::reactor::{Core, Timeout};
 use tokio_core::net::TcpListener;
 
 use hyper::{Get, StatusCode};
-
 use hyper::server::{Http, Service, Request, Response};
-
 use hyper::mime;
 use hyper::header::{ContentType, Connection, AccessControlAllowOrigin};
 use hyper::Chunk;
 
-
-use std::time::{Duration};
-use std::convert::From;
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::time::Duration;
 
 
 enum Case<A,B>{A(A), B(B)}
@@ -45,11 +39,11 @@ where
 }
 
 
-struct EventGen{
-    tx: mpsc::Sender<mpsc::Sender<Result<Chunk,hyper::Error>>>,
+struct EventService {
+    tx_new: mpsc::Sender<mpsc::Sender<Result<Chunk,hyper::Error>>>,
 }
 
-impl Service for EventGen {
+impl Service for EventService {
     type Request = Request;
     type Response = Response;
     type Error = hyper::Error;
@@ -58,141 +52,98 @@ impl Service for EventGen {
     fn call(&self, req: Request) -> Self::Future {
         match (req.method(), req.path()) {
             (&Get, "/") => {
-                let (tx, rx) = mpsc::channel(10);
-                self.tx.clone().send(tx)
-                    .map_err(|_| hyper::Error::Incomplete)//no other errors types allowed
+                let (tx_msg, rx_msg) = mpsc::channel(10);
+                self.tx_new.clone().send(tx_msg)
+                    .map_err(|_| hyper::Error::Incomplete)// other errors types disllowed by hyper
                     .and_then(|_|{
                         Ok(Response::new()
                             .with_status(StatusCode::Ok)
                             .with_header(AccessControlAllowOrigin::Any)
                             .with_header(ContentType(mime::TEXT_EVENT_STREAM))
                             .with_header(Connection::keep_alive())   
-                            .with_body(rx))
-                    }) 
-                    .boxed()    
+                            .with_body(rx_msg))
+                    })
+                    .boxed() 
+            },
 
-            },            
-            _ => {
-                futures::future::ok(Response::new()
-                    .with_status(StatusCode::NotFound))
-                    .boxed()
-            }
+            _ => future_ok(Response::new().with_status(StatusCode::NotFound)).boxed()
         }
     }
+}
+
+// this fn replaces closures to avoid boxing in some cases
+fn print_err<T:std::fmt::Debug>(t:T){
+    println!("{:?}", t);
 }
 
 fn main() {
     pretty_env_logger::init().expect("unable to initialize the env logger");
     let addr = "127.0.0.1:7778".parse().expect("addres parsing failed");
 
-    let mut lp = Core::new().expect("unable to initialize the main event loop");
-    let handle = lp.handle();
-    let sender_handle = lp.handle();
+    let mut core = Core::new().expect("unable to initialize the main event loop");
+    let handle = core.handle();
 
-    let keeper_store:Rc<RefCell<Vec<mpsc::Sender<Result<Chunk, hyper::Error>>>>> = Rc::new(RefCell::new(Vec::new()));
+    let clients:Vec<mpsc::Sender<Result<Chunk, hyper::Error>>> = Vec::new();
+    let (tx_new, rx_new) = mpsc::channel(10);
 
-    let sender_store = keeper_store.clone();
-    let (tx, rx) = mpsc::channel(10);
-/*
-    let keeper = loop_fn((rx, keeper_store), |(rx,store)|{
-        rx.into_future()
-            .and_then(move |(item, rx)|{
-                match item {
-                    Some(item) => {
-                         
-                        store.borrow_mut().push(item); 
-                        println!("abonent {} stored", store.borrow().len());
-                        Ok(Loop::Continue((rx,store)))
-                    },
-                    None =>{
-                        println!("keeper loop stopped"); 
-                        Ok(Loop::Break((rx,store)))
-                    }
-                }        
-            })
-    }).map(|_| ()).map_err(|_| ());
+    let event_delay = Duration::from_secs(2);
 
-    handle.spawn(keeper);
+    let handle2 = core.handle();
+    let fu_to = Timeout::new(event_delay, &handle2).unwrap().map_err(print_err);
+    let fu_rx = rx_new.into_future().map_err(print_err);
 
-
-    let sender = loop_fn(sender_store, move |store|{
-        let tstore = store.clone();
-        let estore = store.clone();
-        
-        Timeout::new(Duration::from_secs(2), &sender_handle).unwrap().map_err(|_| ())
-            .and_then(move |_|{
-                let tx_iter = tstore.borrow().clone().into_iter()
-                    .map(|tx| tx.send(Ok(Chunk::from(format!("event: userconnect\ndata: {{\"username\": \"Frodo\", \"time\": \"{:?}\"}}\n\n", std::time::SystemTime::now())))));
-                futures::future::join_all(tx_iter).map_err(|e| {println!("{:?}", e); ()})
-            })
-            .and_then(move |_|{
-                    Ok(Loop::Continue(store))
-            })
-            .or_else(move |_| Ok(Loop::Continue(estore)))
-    }).map(|()| ());
-
-    handle.spawn(sender);
-*/
-    fn geterr<T:std::fmt::Debug>(t:T){
-        println!("{:?}", t);
-    }
-
-    let fu_to = Timeout::new(Duration::from_secs(2), &sender_handle).unwrap().map_err(geterr);//Item = ()
-    let fu_rx = rx.into_future().map_err(geterr);//Item = (Option<_>, rx)
-
-    let broker = loop_fn((fu_to, fu_rx, sender_store), move |(fu_to, fu_rx, store)|{
-        let ihandle = sender_handle.clone(); 
+    let broker = loop_fn((fu_to, fu_rx, clients), move |(fu_to, fu_rx, mut clients)|{
+        let handle = handle2.clone(); 
         fu_to.select2(fu_rx)
             .map_err(|_| ())// !!!
-            .and_then(move |done|{
+            .and_then(move |done|
                 match done {
-                    Either::A((_, fu_rx)) => Case::A({//sender
-                        
-                        let tx_iter = store.borrow().clone().into_iter()
+                    Either::A((_, fu_rx)) => Case::A({//send messages
+                        let tx_iter = clients.into_iter()
                             .map(|tx| tx.send(Ok(Chunk::from(format!("event: userconnect\ndata: {{\"username\": \"Frodo\", \"time\": \"{:?}\"}}\n\n", std::time::SystemTime::now())))));
-                        futures::future::join_all(tx_iter)
-                            .map_err(geterr)
-                            .and_then(move |_|        
-                                futures::future::ok::<_,()>(Loop::Continue((
-                                        Timeout::new(Duration::from_secs(2), &ihandle).unwrap().map_err(geterr),
+                        futures::stream::futures_unordered(tx_iter)
+                            .map(|x| Some(x))
+                            .or_else(|e| { println!("{:?} client removed", e); Ok::<_,()>(None)})
+                            .filter_map(|x| x)
+                            .collect()
+                                .and_then(move |clients|
+                                    futures::future::ok::<_,()>(Loop::Continue((
+                                        Timeout::new(event_delay, &handle).unwrap().map_err(print_err),
                                         fu_rx, 
-                                        store
-                                )))
+                                        clients
+                                    )))                            
                             )
-                             
+
                     }),
                         
-                    
-                    Either::B(((item, rx), fu_to)) => Case::B({//keeper
+                    Either::B(((item, rx_new), fu_to)) => Case::B({//register new client
                         match item {
                             Some(item) => {
-                                store.borrow_mut().push(item); 
-                                println!("abonent {} stored", store.borrow().len());
+                                clients.push(item); 
+                                println!("client {} registered", clients.len());
                             },
-                            None =>{
-                                println!("keeper loop get None"); 
-                            }
+                            None => println!("keeper loop get None"),
                         }       
 
                         futures::future::ok::<_,()>(Loop::Continue((
                             fu_to,
-                            rx.into_future().map_err(geterr), 
-                            store
+                            rx_new.into_future().map_err(print_err), 
+                            clients
                         )))
                     }),
                 }              
-        })
-    });//.map(|()| ()).map_err(|_| ());
+            )
+    });
 
     handle.spawn(broker);
 
-    let listener = TcpListener::bind(&addr, &lp.handle()).expect("unable to listen");
+    let listener = TcpListener::bind(&addr, &handle).expect("unable to listen");
     println!("Listening on http://{} with 1 thread.", listener.local_addr().expect("local address show"));
+
     let srv = listener.incoming().for_each(move |(stream, addr)| {
-        
-        Http::new().bind_connection(&handle, stream, addr, EventGen{ tx:tx.clone() });
+        Http::new().bind_connection(&handle, stream, addr, EventService{ tx_new: tx_new.clone() });
         Ok(())
     });
 
-    lp.run(srv).expect("error running the event loop");
+    core.run(srv).expect("error running the event loop");
 }
