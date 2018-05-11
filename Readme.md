66a5d2938515873e1a9b1a2817423d03026155eb
Alex <alex@drop.it>
2018-05-11 20:46:45 +0300

    update to hyper v0.11.26

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 # A simple server sent events example 
 
-Written in Rust using [hyper](https://github.com/hyperium/hyper) HTTP library v0.11.0.
+Written in Rust using [hyper](https://github.com/hyperium/hyper) HTTP library v0.11.26.
 
 **Usage:** Just start the server and open the link: http://127.0.0.1:7878/
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 extern crate futures;
 extern crate hyper;
-extern crate pretty_env_logger;
 extern crate tokio_core;
 extern crate bytes;
 
 use bytes::{BytesMut, Bytes, BufMut};
 
-use futures::future::{BoxFuture, Either, ok as future_ok};
-use futures::{Stream,Future,Sink,Poll};
+use futures::IntoFuture;
+use futures::Stream;
+use futures::future::{Future, Either, ok, loop_fn, Loop};
 use futures::sync::mpsc;
-use futures::future::{loop_fn, Loop};
+use futures::sink::Sink;
+
 
 use tokio_core::reactor::Timeout;
 
@@ -21,24 +22,6 @@ use hyper::Chunk;
 
 use std::time::Duration;
 use std::io::Write;
-
-
-enum Case<A,B>{A(A), B(B)}
-
-impl<A,B,I,E> Future for Case<A,B>
-where 
-    A:Future<Item=I, Error=E>,
-    B:Future<Item=I, Error=E>,
-{
-    type Item = I;
-    type Error = E;
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error>{
-        match *self {
-            Case::A(ref mut a) => a.poll(),
-            Case::B(ref mut b) => b.poll(),
-        }
-    }
-}
 
 // this fn replaces closures to avoid boxing in some cases
 fn print_err<T:std::fmt::Debug>(t:T) {
@@ -53,43 +36,42 @@ impl Service for EventService {
     type Request = Request;
     type Response = Response;
     type Error = hyper::Error;
-    type Future = BoxFuture<Response, Self::Error>;
+    type Future = Box<Future<Item=Response, Error=Self::Error>>;
 
     fn call(&self, req: Request) -> Self::Future {
         match (req.method(), req.path()) {
             (&Get, "/events") => { println!("request events");
                 let (tx_msg, rx_msg) = mpsc::channel(10);
-                self.tx_new.clone().send(tx_msg)
-                    .map_err(|_| hyper::Error::Incomplete)// other errors types disallowed by hyper
-                    .and_then(|_|{
-                        Ok(Response::new()
-                            .with_status(StatusCode::Ok)
-                            .with_header(AccessControlAllowOrigin::Any)
-                            .with_header(ContentType(mime::TEXT_EVENT_STREAM))
-                            .with_header(Connection::keep_alive())   
-                            .with_body(rx_msg))
-                    })
-                    .boxed() 
+                Box::new(
+                    self.tx_new.clone().send(tx_msg)
+                        .map_err(|_| hyper::Error::Incomplete)// other errors types disallowed by hyper
+                        .and_then(|_|{
+                            Ok(Response::new()
+                                .with_status(StatusCode::Ok)
+                                .with_header(AccessControlAllowOrigin::Any)
+                                .with_header(ContentType(mime::TEXT_EVENT_STREAM))
+                                .with_header(Connection::keep_alive())   
+                                .with_body(rx_msg))
+                        })
+                        .into_future()
+                )
+                    
             },
 
-            (&Get, "/") => { println!("request html");
-                future_ok(Response::new()
-                    .with_status(StatusCode::Ok)
-                    .with_body(HTML))
-                    .boxed()
+            (&Get, "/") => { 
+                println!("request html");
+                Box::new(ok(Response::new().with_status(StatusCode::Ok).with_body(HTML)))
             }
 
-            (method, path) => { println!("invalid request method: {:?}, path: {:?}", method, path);
-                future_ok(Response::new()
-                    .with_status(StatusCode::NotFound))
-                    .boxed()
+            (method, path) => { 
+                println!("invalid request method: {:?}, path: {:?}", method, path);
+                Box::new(ok(Response::new().with_status(StatusCode::NotFound)))
             }
         }
     }
 }
 
 fn main() {
-    pretty_env_logger::init().expect("unable to initialize the env logger");
     let addr = "127.0.0.1:7878".parse().expect("addres parsing failed");
 
     let (tx_new, rx_new) = mpsc::channel(10);
@@ -111,7 +93,7 @@ fn main() {
             .map_err(|_| ())
             .and_then(move |done|
                 match done {
-                    Either::A((_, fu_rx)) => Case::A({//send messages
+                    Either::A((_, fu_rx)) => Either::A({//send messages
                         let mut buf = BytesMut::with_capacity(512).writer();
                         write!(buf, "event: uptime\ndata: {{\"number\": \"{}\", \"time\": \"{}\"}}\n\n", event_counter, start_time.elapsed().as_secs()).expect("msg write failed");
                         let msg:Bytes = buf.into_inner().freeze();
@@ -123,7 +105,7 @@ fn main() {
                             .filter_map(|x| x)
                             .collect()
                             .and_then(move |clients|
-                                future_ok(Loop::Continue((
+                                ok(Loop::Continue((
                                     Timeout::new(event_delay, &handle).unwrap().map_err(print_err),
                                     fu_rx, 
                                     clients,
@@ -132,7 +114,7 @@ fn main() {
                             )
                     }),
                         
-                    Either::B(((item, rx_new), fu_to)) => Case::B({//register new client
+                    Either::B(((item, rx_new), fu_to)) => Either::B({//register new client
                         match item {
                             Some(item) => {
                                 clients.push(item); 
@@ -141,7 +123,7 @@ fn main() {
                             None => println!("keeper loop get None"),
                         }       
 
-                        future_ok(Loop::Continue((
+                        ok(Loop::Continue((
                             fu_to,
                             rx_new.into_future().map_err(print_err), 
                             clients,
